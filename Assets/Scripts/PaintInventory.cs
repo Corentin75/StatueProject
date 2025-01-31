c956dacf8afdd005c965f200aade160bc4acd8de
Lordsamourai <157367778+Lordsamourai@users.noreply.github.com>
2025-01-31 16:52:28 +0100

    update

--- Assets/Scripts/PaintInventory.cs
+++ Assets/Scripts/PaintInventory.cs
@@ -1,27 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.XR;
 
 public class PaintInventory : MonoBehaviour
 {
     private int maxCapacity;
     private int currentCapacity;
 
+    private float scaleDiminution;
+
+    public float duration = 10f;
+
+    public GameObject paintTank;
+
     // Start is called before the first frame update
     void Start()
     {
         maxCapacity = 10;
         currentCapacity = maxCapacity;
+
+        scaleDiminution = 1 / maxCapacity;
+
+        StartCoroutine(ReduceScaleOverTime());
     }
 
     // Update is called once per frame
-    void Update()
+    IEnumerator ReduceScaleOverTime()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        float elapsedTime = 0f;
+        Vector3 initialScale = paintTank.transform.localScale;
+        Vector3 targetScale = new Vector3(1, scaleDiminution, 1); // Objectif
+
+        while (elapsedTime < duration)
         {
-            currentCapacity -= 1;
-            Debug.Log("Peinture actuelle : " + currentCapacity);
+            elapsedTime += Time.deltaTime;
+            float progress = elapsedTime / duration;
+
+            // Interpolation entre la taille initiale et la taille minimale
+            paintTank.transform.localScale = Vector3.Lerp(initialScale, targetScale, progress);
+
+            yield return null; // Attendre la frame suivante
         }
+
+        // S'assurer que l'objet atteint bien la taille minimale
+        paintTank.transform.localScale = targetScale;
     }
 
     //public void Vidage()
