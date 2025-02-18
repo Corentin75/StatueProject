b38f1b40f1d73e73603c5596c7b8ffe88b2e86cf
enzB29 <enzo.bordet43@gmail.com>
2025-02-18 15:02:08 +0100

    diffuculty mode

---
+++ Assets/Scripts/rotation.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    public DifficultySettings difficultySettings;
+    private float speed;
+    private int direction = 1;
+
+    void Start()
+    {
+        speed = difficultySettings.GetRotationSpeed();
+    }
+
+    void Update()
+    {
+        if (difficultySettings.ShouldReverseRotation())
+        {
+            direction *= -1;
+        }
+        transform.Rotate(Vector3.up * Time.deltaTime * speed * direction);
+    }
+
+    public void ButonClick()
+    {
+        speed = difficultySettings.GetRotationSpeed();
+        
+    }
+}
