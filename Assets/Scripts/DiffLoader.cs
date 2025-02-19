913c7fb44d379a92bc8b40aa0d8e8594434688a2
enzB29 <enzo.bordet43@gmail.com>
2025-02-19 15:27:41 +0100

    GameLoader (seulement avec des débug log pour l'instant

--- Assets/Scripts/DiffLoader.cs
+++ Assets/Scripts/DiffLoader.cs
@@ -6,16 +6,7 @@ public class DiffLoader : MonoBehaviour
 {
     [SerializeField] public DifficultySettings difficultySettings;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
     public void setModeEasy()
     {
         difficultySettings.SetDifficulty(DifficultySettings.DifficultyLevel.Easy);
