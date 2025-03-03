5a5132c7b542518bece8ba8d3b93c43beab9d3db
Corentin75 <corentindetournay05@gmail.com>
2025-03-03 18:24:26 +0100

    3 mars, ajout atelier et réglage lancement jeu
    TODO: tp game

--- Assets/Scripts/DiffLoader.cs
+++ Assets/Scripts/DiffLoader.cs
@@ -7,15 +7,30 @@ public class DiffLoader : MonoBehaviour
     [SerializeField] public DifficultySettings difficultySettings;
     public void setModeEasy()
     {
+        if (difficultySettings == null)
+        {
+            Debug.LogError("DifficultySettings is NULL! Make sure it is assigned in the Inspector.");
+            return;
+        }
         difficultySettings.SetDifficulty(DifficultySettings.DifficultyLevel.Easy);
     }
     public void setModeHard()
     {
+        if (difficultySettings == null)
+        {
+            Debug.LogError("DifficultySettings is NULL! Make sure it is assigned in the Inspector.");
+            return;
+        }
         difficultySettings.SetDifficulty(DifficultySettings.DifficultyLevel.Hard);
     }
 
     public void setModeNormal()
     {
+        if (difficultySettings == null)
+        {
+            Debug.LogError("DifficultySettings is NULL! Make sure it is assigned in the Inspector.");
+            return;
+        }
         difficultySettings.SetDifficulty(DifficultySettings.DifficultyLevel.Normal);
     }
 }
