220e1beddeea15e0651149e60577539e2c85e2bb
Corentin75 <corentindetournay05@gmail.com>
2025-03-03 18:37:12 +0100

    player tp to atelier

--- Assets/Scripts/GameLoader.cs
+++ Assets/Scripts/GameLoader.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class GameLoader : MonoBehaviour
 {
     [SerializeField] public DifficultySettings difficultySettings;
+    public GameObject player;
 
     public void LoadGame()
     {
         if (difficultySettings != null)
         {
+            player.transform.position = new Vector3(7.1f, 0, 3.8f);
+            player.transform.rotation = Quaternion.Euler(new Vector3());
+
             switch (difficultySettings.difficulty)
             {
                 case DifficultySettings.DifficultyLevel.Easy:
