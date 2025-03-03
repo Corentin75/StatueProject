eb142c7a3c47e797b759f06445909ec247a6a307
Lordsamourai <157367778+Lordsamourai@users.noreply.github.com>
2025-03-03 18:28:46 +0100

    Changement CanvaConfirm

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -15,8 +15,9 @@ public class LevelManager : MonoBehaviour
     public GameObject socle;
     private GameObject currentStructure;
     public GameObject canvaConfirm;
-    public GameObject canvaChoice;
+    public GameObject choice;
 
+    public Button currentButton;
 
     public Button[] buttonsChoice;
 
@@ -79,26 +80,25 @@ public class LevelManager : MonoBehaviour
 
     private IEnumerator WaitAndIncreaseLevel()
     {
-        yield return new WaitForSeconds(10f); // Attend 10 secondes
+        yield return new WaitForSeconds(30f); // Attend 10 secondes
 
         UpdateLevel();
     }
 
     public void OnCliCk(Button button)
     {
-        Debug.Log("Bouton actuelle : " + button);
-
-        Transform parentTransform = button.transform;
-
-        Quaternion instanceRotation = Quaternion.identity;
-
-        Instantiate(canvaConfirm, soclePosition, instanceRotation, parentTransform);
-    }
-
-    private void OpenConfirm()
-    {
         canvaConfirm.SetActive(true);
+
+        currentButton = button;
     }
+
+    public void OpenConfirm()
+    {
+        Debug.Log("Vous avez choise : " + currentButton);
+        canvaConfirm.SetActive(false);
+    }
+
+
     public void CloseConfirm()
     {
         canvaConfirm.SetActive(false);
